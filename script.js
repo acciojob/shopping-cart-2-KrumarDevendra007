6ed392bf2274394253f995e347df0d3341a732a5
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-05-29 13:24:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+
+let totalPrice = 0;
+
+function grandPrice() {
+    const nameInput = document.getElementById("item-name-input");
+    const priceInput = document.getElementById("item-price-input");
+    const price = parseFloat(priceInput.value);
+	totalPrice +=  price;
+    document.getElementById("total-price").innerText = totalPrice;
+
+     nameInput.value = "";
+     priceInput.value = "";
+}
+
